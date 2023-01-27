d16d00eb41fb0f656aa20bbac80db7f19fe6a47f
Aishwary Tripathi <aishwarytripathi2001@gmail.com>
2023-01-27 20:20:36 +0530

    modified for production

--- backend/index.mjs
+++ backend/index.mjs
@@ -14,6 +14,13 @@ app.use(cors({origin: `http://localhost:3000`}));
 
 app.use('/api/', PostRouter);
 
+if (process.env.NODE_ENV === 'production') {
+    //*Set static folder up in production
+    app.use(express.static('../frontend/build'));
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, '../frontend', 'build','index.html')));
+  }
+
 const port = process.env.PORT || 8080;
 mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser: true});
 app.listen(port, ()=>{
